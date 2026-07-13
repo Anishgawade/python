2b3af3cd0fa6eb09f600da67832785e488adf915
Anishgawade <anishgawade495@gmail.com>
2026-07-13 20:03:23 +0530

    Print statements in Python

---
+++ print_stm.py
@@ -0,0 +1,21 @@
+name = "Anish"
+age = 22
+cgpa = 7.29
+gender = "male"
+
+# print("Hello", name , "your age is", age, "your gender is " , gender)
+
+# print("Hello" + name + "your age is", age, "your gender is " + gender)
+
+# we cannot use + to print str and int together 
+# but we can use , for any datatype
+
+# print(name, age, cgpa, sep="#")
+
+# print(name, end=" ")
+# print(age)
+# print(cgpa)
+
+# F-string
+
+print(f"Your name is{name}, and your age is {age-10}")
