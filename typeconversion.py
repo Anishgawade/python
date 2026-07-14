361599123810aaf11266a7159253c2cb44a8d37e
Anishgawade <anishgawade495@gmail.com>
2026-07-14 19:48:05 +0530

    Type conversion in Python

---
+++ typeconversion.py
@@ -0,0 +1,24 @@
+# x = 2
+# y = 3.0
+
+# z = x+y
+
+# print(z)
+# print(type(z))
+
+# Explicit conversion
+
+x = "100"
+y = "200"
+
+# z = int(x)
+# w = int(y)
+
+# print(z+w)
+# print(type(z+w))
+
+# without extra variable
+
+z = print(int(x)+ int(y))
+
+print(type(z))
